99eea917e0a40f3999fd06390043e06e4124f9d3
Gerardo Wacker <ge@rar.vg>
2022-03-26 21:13:36 -0300

    Created initial code. Will develop on it later.

--- src/damas/Start.java
+++ src/damas/Start.java
@@ -1,2 +1,15 @@
-package damas;public class Start {
+package damas;
+
+import damas.instance.Damas;
+
+public class Start
+{
+   // Creates a new instance for the main game.
+   private static final Damas damas = new Damas();
+
+   public static void main(String[] args)
+   {
+      // Launches the game
+      damas.launch();
+   }
 }
--- src/damas/instance/Damas.java
+++ src/damas/instance/Damas.java
@@ -1,2 +1,16 @@
-package damas.instance;public class Damas {
+package damas.instance;
+
+/**
+ * Damas, a checkers game.
+ * Made as a final project for Programming class.
+ * @author Gerardo Wacker
+ * @author Juan Ignacio Vecchio
+ */
+public class Damas
+{
+   /**
+    * Launches the game
+    */
+   public void launch()
+   {}
 }
