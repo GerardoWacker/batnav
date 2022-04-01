65c994fb60542e18e5995fe03af86801b29b95f2
Gerardo Wacker <ge@rar.vg>
2022-04-01 10:23:23 -0300

    Added Rest utils into the project

--- src/damas/utils/RestUtils.java
+++ src/damas/utils/RestUtils.java
@@ -1,2 +1,109 @@
-package damas.utils;public class RestUtils {
+package damas.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.util.function.Consumer;
+
+public class RestUtils {
+
+   private final String REST_ENDPOINT;
+   private final Sambayon sambayon;
+
+   /**
+    * Helps handle requests to the static server more efficiently.
+    * @param sambayon Sambayón geolocation manager.
+    */
+   public RestUtils(final Sambayon sambayon) {
+      this.sambayon = sambayon;
+      this.REST_ENDPOINT = sambayon.getServer("damas") + "/";
+   }
+
+   /**
+    * Sends a request including a JSON object to the specified endpoint.
+    * @param endpoint Requested endpoint to access.
+    * @param obj JSON object to send.
+    * @param callback Response from server (String).
+    */
+   public void sendJsonRequest(String endpoint, JSONObject obj, Consumer<String> callback) {
+      try {
+         HttpClient httpclient = HttpClients.createDefault();
+         StringEntity requestEntity = new StringEntity(obj.toString(), ContentType.APPLICATION_JSON);
+
+         HttpPost postMethod = new HttpPost(this.REST_ENDPOINT + endpoint);
+         postMethod.setEntity(requestEntity);
+
+         HttpResponse response = httpclient.execute(postMethod);
+         HttpEntity entity = response.getEntity();
+
+         if (entity != null) {
+            String responseString = EntityUtils.toString(entity);
+            callback.accept(responseString);
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   /**
+    * Sends a request including a String to the specified endpoint.
+    * @param endpoint Requested endpoint to access.
+    * @param obj JSON string to send.
+    * @param callback Response from server (JsonObject).
+    */
+   public void sendJSONRequest(String endpoint, JSONObject obj, Consumer<JsonObject> callback) {
+      try {
+         HttpClient httpclient = HttpClients.createDefault();
+         StringEntity requestEntity = new StringEntity(obj.toString(), ContentType.APPLICATION_JSON);
+
+         HttpPost postMethod = new HttpPost(this.REST_ENDPOINT + endpoint);
+         postMethod.setEntity(requestEntity);
+
+         HttpResponse response = httpclient.execute(postMethod);
+         HttpEntity entity = response.getEntity();
+
+         if (entity != null) {
+            String responseString = EntityUtils.toString(entity);
+            System.out.println(responseString);
+            callback.accept(new Gson().fromJson(responseString, JsonObject.class));
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   /**
+    * Sends a request including a String to the specified endpoint.
+    * @param endpoint Requested endpoint to access.
+    * @param obj JSON object to send.
+    * @param callback Response from server (String).
+    */
+   public void sendJsonRequest(String endpoint, String obj, Consumer<String> callback){
+      try {
+         HttpClient httpclient = HttpClients.createDefault();
+         StringEntity requestEntity = new StringEntity(obj, ContentType.APPLICATION_JSON);
+
+         HttpPost postMethod = new HttpPost(this.REST_ENDPOINT + endpoint);
+         postMethod.setEntity(requestEntity);
+
+         HttpResponse response = httpclient.execute(postMethod);
+         HttpEntity entity = response.getEntity();
+
+         if (entity != null) {
+            String responseString = EntityUtils.toString(entity);
+            callback.accept(responseString);
+         }
+      }catch (Exception e){
+         e.printStackTrace();
+      }
+   }
 }
