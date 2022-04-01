f401a273f2909b2e7d5f2151196fe6c4ecdafe7d
Gerardo Wacker <ge@rar.vg>
2022-04-01 11:54:31 -0300

    Added some code into the Game's main class

--- src/damas/instance/Damas.java
+++ src/damas/instance/Damas.java
@@ -1,18 +1,45 @@
 package damas.instance;
 
-/**
- * Damas, a checkers game.
- * Made as a final project for Programming class.
- * @author Gerardo Wacker
- * @author Juan Ignacio Vecchio
- */
+import damas.config.ConfigManager;
+import damas.online.session.SessionManager;
+import damas.utils.Logger;
+import damas.utils.RestUtils;
+import damas.utils.Sambayon;
+
 public class Damas
 {
+   private final Sambayon sambayon;
+   private final RestUtils restUtils;
+   private final SessionManager sessionManager;
+   private final ConfigManager configManager;
+
    /**
-    * Launches the game
+    * Damas, a checkers game.
+    * Made as a final project for Programming class.
+    * @author Gerardo Wacker
+    * @author Juan Ignacio Vecchio
+    */
+   public Damas()
+   {
+      this.sambayon = new Sambayon();
+      this.configManager = new ConfigManager();
+      this.restUtils = new RestUtils(this.sambayon);
+      this.sessionManager = new SessionManager(this.restUtils, this.configManager);
+   }
+
+   /**
+    * Launches the game.
     */
    public void launch()
    {
-      System.out.println("¡Hola mundo!");
+      // Verify if server is accesible
+      if(this.sambayon.isAccesible())
+      {
+         Logger.log("Se pudo establecer una conexión con Sambayón.");
+
+         // Load session
+         this.sessionManager.loadSession();
+
+      }
    }
 }
