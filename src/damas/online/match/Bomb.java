769417b0760b530b9cea34e1cefb4fad325179e6
Gerardo Wacker <ge@rar.vg>
2022-04-01 23:49:18 -0300

    Added Ship and Bomb classes

--- src/damas/online/match/Bomb.java
+++ src/damas/online/match/Bomb.java
@@ -1,2 +1,27 @@
-package damas.online.match;public class Bomb {
+package damas.online.match;
+
+public class Bomb
+{
+   private final int x, y;
+
+   /**
+    * Throwable bomb. Kaboom!
+    * @param x Position in the X axis.
+    * @param y Position in the Y axis.
+    */
+   public Bomb(int x, int y)
+   {
+      this.x = x;
+      this.y = y;
+   }
+
+   /**
+    * Gets the bomb position. Will be used to send information to the server.
+    *
+    * @return The bomb's coordinates.
+    */
+   public int[] getAsRawData()
+   {
+      return new int[]{this.x, this.y};
+   }
 }
--- src/damas/online/match/Ship.java
+++ src/damas/online/match/Ship.java
@@ -1,2 +1,105 @@
-package damas.online.match;public class Ship {
+package damas.online.match;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class Ship
+{
+   private final int size;
+   private int x, y;
+   private boolean vertical;
+   private Status status;
+
+   /**
+    * A ship. Will also be used for UI purposes, and to facilitate data handling.
+    *
+    * @param size Size of the ship to be created
+    */
+   public Ship(int size)
+   {
+      this.size = size;
+      this.status = Status.ALIVE;
+   }
+
+   /**
+    * Sets the ship position;
+    *
+    * @param x        Position in the X axis.
+    * @param y        Position in the Y axis.
+    * @param vertical Is the ship vertically positioned
+    */
+   public void setPosition(final int x, final int y, final boolean vertical)
+   {
+      this.x = x;
+      this.y = y;
+      this.vertical = vertical;
+   }
+
+   /**
+    * Gets the ship position. Will be used to send information to the server.
+    *
+    * @return A list of several coordinates.
+    */
+   public List<int[]> getAsRawData()
+   {
+      final List<int[]> rawData = Lists.newArrayList();
+
+      // Populates `rawData` accordingly.
+      if (this.vertical)
+      {
+         for (int i = 0; i < this.size; i++)
+         {
+            rawData.add(new int[]{this.x, this.y + i});
+         }
+      } else
+      {
+         for (int i = 0; i < this.size; i++)
+         {
+            rawData.add(new int[]{this.x + i, this.y});
+         }
+      }
+      return rawData;
+   }
+
+   /**
+    * Sets the ship as dead.
+    */
+   public void setDead()
+   {
+      this.status = Status.DEAD;
+   }
+
+   public int getSize()
+   {
+      return size;
+   }
+
+   public int getX()
+   {
+      return x;
+   }
+
+   public int getY()
+   {
+      return y;
+   }
+
+   public boolean isVertical()
+   {
+      return vertical;
+   }
+
+   public Status getStatus()
+   {
+      return status;
+   }
+
+   /**
+    * Ship status.
+    */
+   enum Status
+   {
+      ALIVE, DEAD
+   }
 }
