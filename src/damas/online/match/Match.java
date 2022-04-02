4142bf0e211d0d63d70cb594f77cb18961860f2c
Gerardo Wacker <ge@rar.vg>
2022-04-01 23:49:48 -0300

    Started development for Match management.

--- src/damas/online/match/Match.java
+++ src/damas/online/match/Match.java
@@ -1,2 +1,23 @@
-package damas.online.match;public class Match {
+package damas.online.match;
+
+import com.google.common.collect.Lists;
+import damas.online.session.User;
+
+import java.util.List;
+
+public class Match
+{
+   private final String id;
+   private final User opponent;
+
+   private final List<Bomb> playerBombs, opponentBombs;
+
+   public Match(String id, User opponent)
+   {
+      this.id = id;
+      this.opponent = opponent;
+      this.playerBombs = Lists.newArrayList();
+      this.opponentBombs = Lists.newArrayList();
+   }
+
 }
--- src/damas/online/match/MatchManager.java
+++ src/damas/online/match/MatchManager.java
@@ -1,2 +1,13 @@
-package damas.online.match;public class MatchManager {
+package damas.online.match;
+
+import damas.online.socket.Connection;
+
+public class MatchManager
+{
+   private final Connection connection;
+
+   public MatchManager(Connection connection)
+   {
+      this.connection = connection;
+   }
 }
