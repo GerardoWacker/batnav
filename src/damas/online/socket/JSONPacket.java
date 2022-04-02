e043e62bc6a66e5126d68bf83f4c31eadf09570b
Gerardo Wacker <ge@rar.vg>
2022-04-01 23:48:38 -0300

    Added JSONPacket class

---
+++ src/damas/online/socket/JSONPacket.java
@@ -0,0 +1,27 @@
+package damas.online.socket;
+
+import org.json.JSONObject;
+
+public class JSONPacket extends Packet
+{
+   private final JSONObject packetContent;
+
+   /**
+    * Network packet, constructed with a name and a JSON content.
+    * @param packetName Name of the packet
+    * @param packetContent JSON object to be sent as content.
+    */
+   public JSONPacket(String packetName, JSONObject packetContent)
+   {
+      super(packetName, packetContent.toString());
+      this.packetContent = packetContent;
+   }
+
+   /**
+    * @return The raw JSON object.
+    */
+   public JSONObject getJSONPacketContent()
+   {
+      return packetContent;
+   }
+}
