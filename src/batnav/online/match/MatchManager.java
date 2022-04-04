62f6d26f932fbc6e4b2b97e92d0313c9185dea4c
Gerardo Wacker <ge@rar.vg>
2022-04-04 18:59:50 -0300

    Added initial methods into MatchManager

--- src/batnav/online/match/MatchManager.java
+++ src/batnav/online/match/MatchManager.java
@@ -1,13 +1,47 @@
 package batnav.online.match;
 
+import batnav.online.session.SessionManager;
 import batnav.online.socket.Connection;
+import batnav.online.socket.JSONPacket;
+import batnav.online.socket.Packet;
+import org.json.JSONObject;
 
 public class MatchManager
 {
-   private final Connection connection;
+   private final SessionManager sessionManager;
 
-   public MatchManager(Connection connection)
+   private Match currentMatch;
+
+   public MatchManager(final SessionManager sessionManager)
    {
-      this.connection = connection;
+      this.sessionManager = sessionManager;
+   }
+
+   /**
+    * Sends a packet joining the matchmaking queue.
+    * @param connection Real-time connection manager.
+    */
+   public void joinRankedQueue(final Connection connection)
+   {
+      connection.sendPacket(new Packet("join-ranked-queue", this.sessionManager.getSessionId()));
+   }
+
+   /**
+    * Sends a packet leaving the matchmaking queue.
+    * @param connection Real-time connection manager.
+    */
+   public void leaveRankedQueue(final Connection connection)
+   {
+      connection.sendPacket(new Packet("leave-ranked-queue", this.sessionManager.getSessionId()));
+   }
+
+   public void setCurrentMatch(Match currentMatch)
+   {
+      this.currentMatch = currentMatch;
+   }
+
+   public Match getCurrentMatch()
+   {
+      return currentMatch;
    }
 }
