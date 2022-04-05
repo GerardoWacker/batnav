8ef8f3dca2e26e235073e8843f522dcd96098730
Gerardo Wacker <ge@rar.vg>
2022-04-05 10:40:13 -0300

    Added parameters to the Bomb object

--- src/batnav/online/match/Bomb.java
+++ src/batnav/online/match/Bomb.java
@@ -3,16 +3,21 @@ package batnav.online.match;
 public class Bomb
 {
    private final int x, y;
+   private final boolean opponent, hasHit;
 
    /**
     * Throwable bomb. Kaboom!
     * @param x Position in the X axis.
     * @param y Position in the Y axis.
+    * @param opponent Is the bomb from the opponent.
+    * @param hasHit Has the bomb hit a target.
     */
-   public Bomb(int x, int y)
+   public Bomb(int x, int y, boolean opponent, boolean hasHit)
    {
       this.x = x;
       this.y = y;
+      this.opponent = opponent;
+      this.hasHit = hasHit;
    }
 
    /**
