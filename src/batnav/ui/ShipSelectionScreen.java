0acafbb6653d5a60d2ae9ec293cd6276f4095d50
Gerardo Wacker <ge@rar.vg>
2022-04-12 11:52:56 -0300

    Added a base for ShipSelectionScreen

--- src/batnav/ui/ShipSelectionScreen.java
+++ src/batnav/ui/ShipSelectionScreen.java
@@ -1,6 +1,5 @@
 package batnav.ui;
 
-import batnav.online.match.Bomb;
 import batnav.online.match.Ship;
 import com.google.common.collect.Lists;
 
@@ -10,13 +9,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-public class ShipSelecionScreen extends JFrame implements ActionListener
+public class ShipSelectionScreen extends JFrame implements ActionListener
 {
    protected List<Ship> ships;
    private Ship selectedShip;
    private final ShipSelectionBoard shipSelectionBoard;
 
-   public ShipSelecionScreen()
+   public ShipSelectionScreen()
    {
       this.ships = Lists.newArrayList();
       this.shipSelectionBoard = new ShipSelectionBoard(this);
@@ -28,16 +27,20 @@ public class ShipSelecionScreen extends JFrame implements ActionListener
       this.ships.add(new Ship(3));
       this.ships.add(new Ship(3));
       this.ships.add(new Ship(3));
-      
+      this.ships.add(new Ship(4));
+      this.ships.add(new Ship(5));
    }
 
    // When a ship is selected in the East JPanel, this::selectedShip should be set with its value.
+   // For every ship added into this::ships, draw an Image into the East JPanel for selection.
+   // When it has been clicked, set the selected ship as ships.get(JLabel Id.).
+   // Handle rotation and clicking on board is yet to be implemented, however, it's just a button.
 
    @Override
    public void actionPerformed(ActionEvent e)
    {
       // Get the new mouse coordinates and handle the click.
-      Point point = ShipSelecionScreen.this.getMousePosition();
+      Point point = ShipSelectionScreen.this.getMousePosition();
 
       if (point != null)
       {
