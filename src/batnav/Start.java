e8dd05dc950ab8fb3289393b77b85c72f1d68634
Gerardo Wacker <ge@rar.vg>
2022-04-19 22:11:22 -0300

    Now Start creates the main instance, that way it can be shared in the future through Game::getInstance

--- src/batnav/Start.java
+++ src/batnav/Start.java
@@ -4,20 +4,17 @@ import batnav.instance.Game;
 
 public class Start
 {
-   // Creates a new instance for the main game.
-   private static final Game damas = new Game();
-
    public static void main(String[] args)
    {
       System.out.println("===============================================================================");
-      System.out.println("-- Damas --");
+      System.out.println("-- batnav --");
       System.out.println("El juego se está iniciando...");
-      System.out.println("Para más información, visitá https://www.github.com/gerardowacker/damas.");
+      System.out.println("Para más información, visitá https://www.github.com/gerardowacker/batnav.");
       System.out.println("===============================================================================");
       // Launches the game
       try
       {
-         damas.launch();
+         Game.getInstance().launch();
       } catch (Exception e)
       {
          e.printStackTrace();
