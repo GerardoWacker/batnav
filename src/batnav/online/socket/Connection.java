3f254863e7c10158f7b3978ce6317965b9f8130a
Gerardo Wacker <ge@rar.vg>
2022-04-26 10:55:39 -0300

    Added a connection between `Connection` and `MatchScreen::setPlayerReady`

--- src/batnav/online/socket/Connection.java
+++ src/batnav/online/socket/Connection.java
@@ -63,9 +63,8 @@ public class Connection
             this.socket.on("match", this::match);
             this.socket.on("match-bomb-thrown", this.matchManager::hasThrownBomb);
             this.socket.on("match-bomb-receive", this.matchManager::receiveBomb);
-            this.socket.on("match-ships-set", data -> {
-               this.matchManager.getCurrentMatch().getMatchScreen().repaint();
-            });
+            this.socket.on("match-ships-set", data -> this.matchManager
+                 .getCurrentMatch().getMatchScreen().setPlayerReady());
             this.socket.on("match-ships-receive", this.matchManager::receiveShips);
 
          });
