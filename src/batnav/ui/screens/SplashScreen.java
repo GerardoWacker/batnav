3fc33522541841d0b7fbe3b6944619ac46285aab
Gerardo Wacker <ge@rar.vg>
2022-05-22 15:04:45 -0300

    Added a rotating ship into the SplashScreen

--- src/batnav/ui/screens/SplashScreen.java
+++ src/batnav/ui/screens/SplashScreen.java
@@ -1,5 +1,6 @@
 package batnav.ui.screens;
 
+import batnav.ui.components.RotatingImageLabel;
 import batnav.utils.Colour;
 
 import javax.imageio.ImageIO;
@@ -7,6 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -20,17 +22,16 @@ public class SplashScreen extends JFrame implements ActionListener
    {
 
       super("batnav");
-      this.setType(Type.UTILITY);
       this.setSize(300, 400);
       this.setLocationRelativeTo(null);
       this.setAlwaysOnTop(true);
-      //this.setUndecorated(true);
+      this.setUndecorated(true);
       this.setDefaultCloseOperation(EXIT_ON_CLOSE);
       this.setResizable(false);
 
       //JPanel contentPane = (JPanel) this.getContentPane();
       //contentPane.setBackground(Colour.AliceBlue);
-      this.setLayout(new GridLayout(3,1));
+      this.setLayout(new GridLayout(3, 1));
       this.logoContainerLabel = new JLabel("batnav", SwingConstants.CENTER);
       this.logoContainerLabel.setFont(new Font("San Francisco Display", Font.BOLD, 25));
       this.logoContainerLabel.setHorizontalTextPosition(JLabel.CENTER);
@@ -38,23 +39,26 @@ public class SplashScreen extends JFrame implements ActionListener
       this.splashText.setFont(new Font("San Francisco Display", Font.PLAIN, 15));
       this.splashText.setHorizontalAlignment(JLabel.CENTER);
 
-      JLabel loaderLabel = new JLabel();
+      this.add(this.logoContainerLabel, BorderLayout.CENTER);
 
-      try {
-         Image logo = ImageIO.read(new File("assets/textures/loadingLogo.png"));
-         Image logoScaled = logo.getScaledInstance(60, 60, Image.SCALE_SMOOTH);
-         loaderLabel.setIcon(new ImageIcon(logoScaled));
-      } catch (IOException e) {
+      try
+      {
+         BufferedImage logo = ImageIO.read(new File("assets/ships/ship2.png"));
+         Image logoScaled = logo.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
+         RotatingImageLabel loaderLabel = new RotatingImageLabel(logoScaled, 2);
+         loaderLabel.setHorizontalAlignment(JLabel.CENTER);
+         this.add(loaderLabel, BorderLayout.CENTER);
+         
+         // Start the rotating animation.
+         loaderLabel.start();
+      } catch (IOException e)
+      {
          throw new RuntimeException(e);
       }
 
-      loaderLabel.setHorizontalAlignment(JLabel.CENTER);
-      this.add(this.logoContainerLabel, BorderLayout.CENTER);
-      this.add(loaderLabel, BorderLayout.CENTER);
       this.add(this.splashText);
       this.setVisible(true);
    }
-
 
 
    @Override
@@ -75,7 +79,8 @@ public class SplashScreen extends JFrame implements ActionListener
       this.repaint();
    }
 
-   public static void main(String[] args) {
+   public static void main(String[] args)
+   {
       SplashScreen mysplashscreen = new SplashScreen();
    }
 }
