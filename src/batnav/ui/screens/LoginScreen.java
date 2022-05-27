ac63b37df362a9a125e463ccceee962eb8529caa
Juan Ignacio Vecchio <vecchiojuanignacio@gmail.com>
2022-05-27 12:08:00 -0300

    Update `LoginScreen`

--- src/batnav/ui/screens/LoginScreen.java
+++ src/batnav/ui/screens/LoginScreen.java
@@ -8,82 +8,125 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LoginScreen extends JFrame implements ActionListener
-{
-   private JTextField userName;
-   private JTextField userPassword;
+public class LoginScreen extends JFrame implements ActionListener {
+    private JTextField userName;
+    private JTextField userPassword;
+    private JLabel userLabel;
+    private JLabel logoContainerLabel;
+    private JLabel PasswordLabel;
+    private JButton loginButton;
+    private JLabel altert;
+    private JPanel contentPanel;
+    private JLabel loadingText;
+    private JButton tempButton;
 
-   public LoginScreen()
-   {
-      this.setSize(300, 500);
-      this.setLocationRelativeTo(null);
-      JPanel panel = new JPanel();
-      this.add(panel);
-      panel.setLayout(null);
+    public LoginScreen() {
+        this.setSize(300, 500);
+        this.setLocationRelativeTo(null);
+        this.contentPanel = new JPanel();
+        this.add(contentPanel);
+        contentPanel.setLayout(null);
 
-      JLabel logoContainerLabel = new JLabel("batnav", SwingConstants.CENTER);
-      logoContainerLabel.setFont(new Font("San Francisco Display", Font.BOLD, 25));
-      logoContainerLabel.setHorizontalTextPosition(JLabel.CENTER);
-      logoContainerLabel.setBounds(50,10,200,80);
-      panel.add(logoContainerLabel);
-
-      JLabel userLabel = new JLabel("Usuario");
-      userLabel.setBounds(50,100,80,25);
-      panel.add(userLabel);
-
-      this.userName = new JTextField(20);
-      userName.setBounds(50,130,165,25);
-      panel.add(userName);
-
-      JLabel PasswordLabel = new JLabel("Contraseña");
-      PasswordLabel.setBounds(50,200,80,25);
-      panel.add(PasswordLabel);
-
-      this.userPassword = new JPasswordField(20);
-      panel.add(userPassword);
-      userPassword.setBounds(50,230,165,25);
-
-      JButton button = new JButton("Login");
-      button.setBounds(50,300,165,25);
-      button.setActionCommand("login");
-      panel.add(button);
-
-      JLabel prueba = new JLabel("Contraseña");
-      prueba.setOpaque(true);
-      prueba.setForeground(Color.white);
-      prueba.setBackground(Color.red);
-      prueba.setBounds(50,350,80,25);
-      panel.add(prueba);
-
-      this.setResizable(false);
-      this.setAlwaysOnTop(true);
-      this.setVisible(true);
-
-   }
+        this.logoContainerLabel = new JLabel("batnav", SwingConstants.CENTER);
+        logoContainerLabel.setFont(new Font("San Francisco Display", Font.BOLD, 25));
+        logoContainerLabel.setHorizontalTextPosition(JLabel.CENTER);
+        logoContainerLabel.setBounds(50, 10, 200, 80);
 
 
+        this.userLabel = new JLabel("Usuario");
+        userLabel.setBounds(50, 100, 80, 25);
 
-   public static void main(String[] args) {
-      new LoginScreen();
-   }
 
-   @Override
-   public void actionPerformed(ActionEvent e) {
-      final String action = e.getActionCommand();
-      switch (action)
-      {
-         case "login":
-            try {
-               if(Game.getInstance().getSessionManager().login(userName.getText(), userPassword.getText())){
-                  new MainMenuScreen();
-               }else{
-                  System.out.println("lol");
-               }
-               break;
-            } catch (Exception ex) {
-               ex.printStackTrace();
-            }
+        this.userName = new JTextField(20);
+        userName.setBounds(50, 130, 165, 25);
 
-      }
-   }
+
+        this.PasswordLabel = new JLabel("Contraseña");
+        PasswordLabel.setBounds(50, 200, 80, 25);
+
+
+        this.userPassword = new JPasswordField(20);
+        userPassword.setBounds(50, 230, 165, 25);
+
+        this.loginButton = new JButton("Login");
+        loginButton.setBounds(50, 300, 165, 25);
+        loginButton.addActionListener(this);
+        loginButton.setActionCommand("login");
+
+        this.altert = new JLabel("Contraseña");
+        altert.setOpaque(true);
+        altert.setForeground(Color.white);
+        altert.setBackground(Color.red);
+        altert.setBounds(50, 350, 80, 25);
+
+        this.loadingText = new JLabel("Iniciando sesion");
+        loadingText.setBounds(100, 120, 200, 80);
+
+        this.tempButton = new JButton("Back");
+        tempButton.setBounds(50, 300, 165, 25);
+        tempButton.addActionListener(this);
+        tempButton.setActionCommand("Back");
+
+
+        this.setResizable(false);
+        this.setAlwaysOnTop(true);
+        this.setVisible(true);
+
+        this.paintScreen();
+
+
+    }
+
+    private void showLoadingScreen() {
+        this.contentPanel.removeAll();
+        this.contentPanel.add(loadingText);
+        this.contentPanel.add(tempButton);
+        this.revalidate();
+        this.repaint();
+    }
+
+
+    public static void main(String[] args) {
+        new LoginScreen();
+    }
+
+    public void paintScreen() {
+        this.contentPanel.removeAll();
+        this.contentPanel.add(this.userName);
+        this.contentPanel.add(this.logoContainerLabel);
+        this.contentPanel.add(this.userPassword);
+        this.contentPanel.add(this.userLabel);
+        this.contentPanel.add(this.PasswordLabel);
+        this.contentPanel.add(this.loginButton);
+        this.contentPanel.add(this.altert);
+        this.revalidate();
+        this.repaint();
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        final String action = e.getActionCommand();
+        switch (action) {
+            case "login":
+                this.showLoadingScreen();
+                try {
+                    if (Game.getInstance().getSessionManager().login(userName.getText(), userPassword.getText())) {
+                        new MainMenuScreen();
+                    } else {
+                        System.out.println("lol");
+                    }
+                    break;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case "Back":
+                this.paintScreen();
+                break;
+
+
+        }
+    }
 }
