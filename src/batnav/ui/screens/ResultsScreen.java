c1d70f97d415d6bda33266d6670e4b752af71aeb
Gerardo Wacker <ge@rar.vg>
2022-06-06 18:43:31 -0300

    Added `ActionListener` into `ResultsScreen`

--- src/batnav/ui/screens/ResultsScreen.java
+++ src/batnav/ui/screens/ResultsScreen.java
@@ -1,19 +1,26 @@
 package batnav.ui.screens;
 
+import batnav.instance.Game;
+import batnav.utils.Colour;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
-public class ResultsScreen extends JFrame
+public class ResultsScreen extends JFrame implements ActionListener
 {
    private final boolean victory;
    private final int elo;
+   private final String matchId;
 
-   public ResultsScreen(final boolean victory, final int elo)
+   public ResultsScreen(final boolean victory, final int elo, final String matchId)
    {
       this.victory = victory;
       this.elo = elo;
+      this.matchId = matchId;
 
       this.setLocationRelativeTo(null);
       this.setResizable(false);
@@ -39,7 +46,16 @@ public class ResultsScreen extends JFrame
          difference.setText((victory ? "+" : "-") + elo);
          difference.setHorizontalAlignment(SwingConstants.CENTER);
 
+         final JButton matchInfoButton = new JButton("Ver más información sobre la partida");
+         matchInfoButton.setBorder(BorderFactory.createEmptyBorder());
+         matchInfoButton.setBackground(Colour.Transparent);
+         matchInfoButton.setHorizontalAlignment(SwingConstants.CENTER);
+         matchInfoButton.addActionListener(this);
+         matchInfoButton.setActionCommand("matchInfo");
+
          final JButton returnButton = new JButton("Volver al menú principal");
+         returnButton.addActionListener(this);
+         returnButton.setActionCommand("return");
 
          this.add(iconLabel);
          this.add(title);
@@ -55,6 +71,19 @@ public class ResultsScreen extends JFrame
 
    public static void main(String[] args)
    {
-      new ResultsScreen(true, 30);
+      new ResultsScreen(true, 30, "asdfghjkl");
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent e)
+   {
+      switch (e.getActionCommand())
+      {
+         case "matchInfo":
+            // TODO: Implement MatchReviewScreen.
+            break;
+         case "return":
+            this.setVisible(false);
+      }
    }
 }
