88af45d5403dd8fb7918c3979bef75de64dbadaa
Gerardo Wacker <ge@rar.vg>
2022-06-07 09:52:04 -0300

    Added text fields

---
+++ src/batnav/ui/components/RoundedPasswordField.java
@@ -0,0 +1,64 @@
+package batnav.ui.components;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedPasswordField extends JPasswordField
+{
+   private final int horizontal, vertical;
+   private Shape shape;
+
+   /**
+    * Creates a rounded text field.
+    *
+    * @param size       Field size.
+    * @param vertical   Vertical radius.
+    * @param horizontal Horizontal radius.
+    */
+   public RoundedPasswordField(int horizontal, int vertical, int size)
+   {
+      super(size);
+
+      this.horizontal = horizontal;
+      this.vertical = vertical;
+
+      this.setOpaque(false);
+   }
+
+   /**
+    * Creates a rounded text field.
+    *
+    * @param radius Border radius.
+    * @param size   Field size.
+    */
+   public RoundedPasswordField(int radius, int size)
+   {
+      this(radius, radius, size);
+   }
+
+   @Override
+   protected void paintComponent(Graphics g)
+   {
+      g.setColor(getBackground());
+      g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+      super.paintComponent(g);
+   }
+
+   @Override
+   protected void paintBorder(Graphics g)
+   {
+      g.setColor(getForeground());
+      g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+   }
+
+   @Override
+   public boolean contains(int x, int y)
+   {
+      if (shape == null || !shape.getBounds().equals(getBounds()))
+      {
+         shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, horizontal, vertical);
+      }
+      return shape.contains(x, y);
+   }
+}
