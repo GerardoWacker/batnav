e201b48794bd08edd0bd23be4527363922bf229d
Gerardo Wacker <ge@rar.vg>
2022-06-09 09:53:30 -0300

    Added `GamePanel` main and alternative colours

--- src/batnav/ui/components/GamePanel.java
+++ src/batnav/ui/components/GamePanel.java
@@ -1,10 +1,14 @@
 package batnav.ui.components;
 
+import batnav.utils.Colour;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel
 {
+   private boolean alternative = false;
+
    @Override
    protected void paintComponent(Graphics g)
    {
@@ -15,11 +19,16 @@ public class GamePanel extends JPanel
       g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
       int w = getWidth(), h = getHeight();
 
-      Color color1 = Color.RED;
-      Color color2 = Color.GREEN;
+      Color color1 = alternative ? new Colour(171, 202, 224) : new Colour(30, 66, 158);
+      Color color2 = alternative ? new Colour(240, 248, 255) : new Colour(14, 75, 207);
       GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
 
       g2d.setPaint(gp);
       g2d.fillRect(0, 0, w, h);
+   }
+
+   public void setAlternative(boolean alternative)
+   {
+      this.alternative = alternative;
    }
 }
