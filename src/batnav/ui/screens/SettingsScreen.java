3d5b47b83a86bf35704e4db5a55d47ee9429c303
Juan Ignacio Vecchio <vecchiojuanignacio@gmail.com>
2022-06-13 20:25:21 -0300

    fixed bug when ships changed size when selected

--- src/batnav/ui/screens/SettingsScreen.java
+++ src/batnav/ui/screens/SettingsScreen.java
@@ -1,8 +1,12 @@
 package batnav.ui.screens;
 
 import batnav.instance.Game;
+import batnav.ui.components.GameButton;
+import batnav.ui.components.GamePanel;
+import batnav.utils.Colour;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,7 +14,7 @@ import java.awt.event.ActionListener;
 public class SettingsScreen extends JFrame implements ActionListener
 {
    private CardLayout cl;
-   private JButton logoutButton;
+   private GameButton logoutButton;
    private JPanel mainPanel, settingsPanel;
 
    public SettingsScreen()
@@ -20,17 +24,20 @@ public class SettingsScreen extends JFrame implements ActionListener
       this.setLocationRelativeTo(null);
 
       this.settingsPanel = new JPanel();
-      this.mainPanel = new JPanel();
+      this.mainPanel = new GamePanel();
       this.mainPanel.setLayout(cl);
       this.add(mainPanel);
       mainPanel.add(settingsPanel, "1");
 
       cl.show(mainPanel, "1");
       settingsPanel.setLayout(null);
-
-      this.logoutButton = new JButton("Logout");
-      logoutButton.setBounds(100, 350, 300, 50);
-      logoutButton.setFont(new Font("San Francisco Display", Font.PLAIN, 18));
+      this.settingsPanel.setOpaque(false);
+      this.settingsPanel.setBackground(Colour.Transparent);
+      this.logoutButton = new GameButton("Logout");
+      logoutButton.setCancel(true);
+      logoutButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Colour.Black,Colour.Black));
+      logoutButton.setBounds(100, 30, 300, 50);
+      logoutButton.setFont(new Font("Display Medium", Font.PLAIN, 18));
       logoutButton.addActionListener(e -> {
          Game.getInstance().getSessionManager().setAndSaveSessionId(null);
          Game.getInstance().getConnection().disconnect();
