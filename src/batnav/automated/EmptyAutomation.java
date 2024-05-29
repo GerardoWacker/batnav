935d7dd030807deefcb53c4671c19fd6f961456b
Gerardo Wacker <ge@rar.vg>
2024-05-29 10:23:42 -0300

    Added an `EmptyAutomation` to handle no injections

---
+++ src/batnav/automated/EmptyAutomation.java
@@ -0,0 +1,28 @@
+package batnav.automated;
+
+public class EmptyAutomation implements Automation
+{
+   @Override
+   public void injectLogin()
+   {
+
+   }
+
+   @Override
+   public void injectMainMenu()
+   {
+
+   }
+
+   @Override
+   public void injectSetShips()
+   {
+
+   }
+
+   @Override
+   public void injectBombSequence()
+   {
+
+   }
+}
