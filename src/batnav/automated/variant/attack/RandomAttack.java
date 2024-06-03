e2c374fc5fb6d98b9ca9508f8d990244086994da
Gerardo Wacker <ge@rar.vg>
2024-06-03 11:30:02 -0300

    Applied code styling in `RandomAttack`

--- src/batnav/automated/variant/attack/RandomAttack.java
+++ src/batnav/automated/variant/attack/RandomAttack.java
@@ -5,29 +5,35 @@ import batnav.utils.Logger;
 
 public class RandomAttack extends Attack
 {
-    public boolean[][] bombMatrix = new boolean[10][10];
+   public boolean[][] bombMatrix = new boolean[10][10];
 
-    public RandomAttack() {
-        fillMatrix();
-    }
-    public void fillMatrix(){
-    for(int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+   public RandomAttack()
+   {
+      fillMatrix();
+   }
+
+   public void fillMatrix()
+   {
+      for (int i = 0; i < 10; i++)
+      {
+         for (int j = 0; j < 10; j++)
+         {
             bombMatrix[i][j] = false;
-        }
-    }
-}
-    @Override
-    public void receiveBomb()
-    {
-        int coordX = (int) (Math.random() * 10);
-        int coordY = (int) (Math.random() * 10);
-        if (!bombMatrix[coordX][coordY])
-        {
-            Game.getInstance().getMatchManager().throwBomb(Game.getInstance().getConnection(),
-                   coordX, coordY);
-            bombMatrix[coordX][coordY] = true;
-            Logger.log("Se arrojó una bomba");
-        }
-    }
+         }
+      }
+   }
+
+   @Override
+   public void receiveBomb()
+   {
+      int coordX = (int) (Math.random() * 10);
+      int coordY = (int) (Math.random() * 10);
+      if (!bombMatrix[coordX][coordY])
+      {
+         Game.getInstance().getMatchManager().throwBomb(Game.getInstance().getConnection(),
+                 coordX, coordY);
+         bombMatrix[coordX][coordY] = true;
+         Logger.log("Se arrojó una bomba");
+      }
+   }
 }
