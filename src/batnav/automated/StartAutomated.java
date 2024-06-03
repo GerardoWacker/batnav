54666079a7a67bdd71b37637933d1d49c1808c95
Gerardo Wacker <ge@rar.vg>
2024-06-03 17:17:27 -0300

    Added user launching method for automations
    Used only for testing purposes :eyes:

--- src/batnav/automated/StartAutomated.java
+++ src/batnav/automated/StartAutomated.java
@@ -1,2 +1,18 @@
-package batnav.automated;public class StartAutomated {
+package batnav.automated;
+
+import batnav.automated.variant.AutomationGenerator;
+import batnav.instance.Game;
+
+import java.util.Scanner;
+
+public class StartAutomated
+{
+   public static void main(String[] args)
+   {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Ingresá número de usuario: ");
+      int i = scanner.nextInt();
+      Game.getInstance().launch(AutomationGenerator.generate("usuario" + i,
+              AutomationGenerator.DefenceType.RANDOM, AutomationGenerator.AttackType.RANDOM));
+   }
 }
