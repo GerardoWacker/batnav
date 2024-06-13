28cd3cc7311d94d6bbd1d1d3ccf870dcbb7d884d
Gerardo Wacker <ge@rar.vg>
2024-06-13 20:38:56 -0300

    Modified `StartAutomated` so it's easier to modify strategies

--- src/batnav/automated/StartAutomated.java
+++ src/batnav/automated/StartAutomated.java
@@ -10,9 +10,44 @@ public class StartAutomated
    public static void main(String[] args)
    {
       Scanner scanner = new Scanner(System.in);
-      System.out.println("Ingresá número de usuario: ");
-      int i = scanner.nextInt();
-      Game.getInstance().launch(AutomationGenerator.generate("usuario" + i,
-              AutomationGenerator.DefenceType.RANDOM, AutomationGenerator.AttackType.CROSS_PREF));
+      System.out.println("Ingresá qué estrategia de ataque elegir: ");
+
+      System.out.println("""
+           1) Cruz
+           2) Random
+           3) Cruz con preferencia
+           4) Random con preferencia.\s
+          \s""");
+
+      int a = scanner.nextInt();
+
+      System.out.println("Ingresá qué estrategia de defensa elegir: ");
+
+      System.out.println("""
+           1) Esquinas
+           2) Random
+           3) Centro\s
+          \s""");
+
+      int d = scanner.nextInt();
+
+      AutomationGenerator.AttackType attack = switch (a)
+      {
+         case 1 -> AutomationGenerator.AttackType.CROSS;
+         case 2 -> AutomationGenerator.AttackType.RANDOM;
+         case 3 -> AutomationGenerator.AttackType.CROSS_PREF;
+         case 4 -> AutomationGenerator.AttackType.RANDOM_PREF;
+         default -> null;
+      };
+
+      AutomationGenerator.DefenceType defence = switch (d)
+      {
+         case 1 -> AutomationGenerator.DefenceType.CORNER;
+         case 2 -> AutomationGenerator.DefenceType.RANDOM;
+         case 3 -> AutomationGenerator.DefenceType.MIDDLE;
+         default -> null;
+      };
+
+      Game.getInstance().launch(AutomationGenerator.generate(attack.toString(), defence, attack));
    }
 }
